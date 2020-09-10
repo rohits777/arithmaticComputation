8d8b9fd97a1fa639146f03d372f06cd92b0c8aaa
rohits777 <rohitashinde95@gmail.com>
2020-09-11 00:44:28 +0530

    Added: added values from Dictionary to array

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -4,8 +4,6 @@ read -p "Enter the first value 'a':" a
 read -p "Enter the second value'b':" b
 read -p "Enter the third value 'c':" c
 
-declare -A computation
-
 x=$(( a + b *c ))
       echo "$a + $b * $c = $x"
 y=$(( a * b + c ))
@@ -14,8 +12,15 @@ z=`awk 'BEGIN{printf("%0.2f", '$c' + '$a' / '$b' )}'`
        echo "$c + $a / $b = $z"
 q=$(( a % b + c ))
       echo "$a % $b + $c = $q "
+declare -A computation
  computation[first]=$x
  computation[second]=$y
  computation[Third]=$z
  computation[Fourth]=$q
 echo -e "values: ${computation[@]}\nkeys:${!computation[@]}"
+
+for((count=1; count <= 4; count++))
+do
+	arr[$count]=${operations[$count]}
+done
+echo "Array: ${arr[@]}"
