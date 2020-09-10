029c322815d764ed4371d43032d68efa31c498b0
rohits777 <rohitashinde95@gmail.com>
2020-09-10 18:29:55 +0530

    Added: added program to Compute a*b+c

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -4,4 +4,6 @@ read -p "Enter the first value 'a':" a
 read -p "Enter the second value'b':" b
 read -p "Enter the third value 'c':" c
 x=$(( a + b *c ))
-echo "a + b * c = $x"
+      echo "$a + $b * $c = $x"
+y=$(( a * b + c ))
+      echo "$a * $b +$c = $y"
