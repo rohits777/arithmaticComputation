0657d93e6b103f23eb8870ca89734816a3be4a41
rohits777 <rohitashinde95@gmail.com>
2020-09-10 17:30:46 +0530

    Added: Inputs from user

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -1,1 +1,5 @@
 echo "Welcome to Arithmatic  Computation program"
+ #!/bin/bash
+read -p "Enter the first value 'a':" a
+read -p "Enter the second value'b':" b
+read -p "Enter the third value 'c':" c
