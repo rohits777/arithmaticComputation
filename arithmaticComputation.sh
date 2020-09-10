239ec1695a03a858c778e0e040092b19cad940e9
rohits777 <rohitashinde95@gmail.com>
2020-09-10 23:57:24 +0530

    Added: added Dictionary for every Computation

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -3,6 +3,9 @@ echo "Welcome to Arithmatic  Computation program"
 read -p "Enter the first value 'a':" a
 read -p "Enter the second value'b':" b
 read -p "Enter the third value 'c':" c
+
+declare -A computation
+
 x=$(( a + b *c ))
       echo "$a + $b * $c = $x"
 y=$(( a * b + c ))
@@ -11,3 +14,8 @@ z=`awk 'BEGIN{printf("%0.2f", '$c' + '$a' / '$b' )}'`
        echo "$c + $a / $b = $z"
 q=$(( a % b + c ))
       echo "$a % $b + $c = $q "
+ computation[first]=$x
+ computation[second]=$y
+ computation[Third]=$z
+ computation[Fourth]=$q
+echo -e "values: ${computation[@]}\nkeys:${!computation[@]}"
